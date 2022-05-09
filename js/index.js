3794286d4facd4c4b22bc72cb7a2d56480b60e0d
murimolda <35343729+murimolda@users.noreply.github.com>
2022-05-09 10:59:40 +0300

    fix: change gitignore file

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
-document.addEventListener("DOMContentLoaded", function () {
-  /*Function to find the element that is wider than the body of the page*/
+document.addEventListener('DOMContentLoaded', function () {
+  /*Function to find the element that is wider than the body of the page */
   const docWidth = document.documentElement.offsetWidth;
 
   [].forEach.call(document.querySelectorAll("*"), function (el) {
